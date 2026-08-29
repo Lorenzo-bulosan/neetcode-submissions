bc003cceb925e3c7b9828ff996d9eda5d23f4f50
Lorenzo Bulosan Bulosan <36127677+Lorenzo-bulosan@users.noreply.github.com>
2026-08-29 18:06:19 +0100

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    '''
+    [1,2,3,6,7,8,10], target=13
+
+    slide window start and end, 1,10
+    if target is bigger then increase left, if not right
+    this works because one we are assured 1 correct answer pair so has to be
+    this works because its sorted
+    '''
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        left = 0
+        right = len(numbers)-1
+
+        while left < right:
+
+            currentSum = numbers[left] + numbers[right]
+            if currentSum > target:
+                right -= 1
+            elif currentSum < target:
+                left += 1
+            else:
+                # solution found
+                break
+
+        return [left+1, right+1]
