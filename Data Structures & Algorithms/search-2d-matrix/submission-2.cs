fd5c0f4c88de66e9f34326b40ed1c3061a248682
Lorenzo Bulosan Bulosan <36127677+Lorenzo-bulosan@users.noreply.github.com>
2026-09-11 12:46:06 +0100

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    /*
+    [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
+    plan a) n+logm => sweep to see which array and then binary search
+    plan b) logn + logm => binary search to find which array and then binary search on those
+    */
+
+    // PLAN A)
+    public bool SearchMatrix(int[][] matrix, int target) {
+
+        // find in which subArray it might be in
+        int possibleRow = -1;
+        var isPossible = false;
+        for(int i=0; i<matrix.Length; i++){ // rows
+
+            if(matrix[i][0]<=target && target<=matrix[i][matrix[0].Length-1]){
+
+                isPossible = true;
+                possibleRow = i; // instead of using extra array space just keep track of index
+                break;
+            }
+        }
+
+        if (!isPossible) return false;
+
+        // binary search inside possible array
+        int l = 0;
+        int r = matrix[0].Length-1;
+        int mid;
+        while(l<=r){
+
+            mid = l+(r-l)/2;
+
+            if(matrix[possibleRow][mid] == target) return true;
+            else if(target < matrix[possibleRow][mid]) r = mid-1;
+            else l = mid+1;
+        }
+
+        return false;
+    }
+}
