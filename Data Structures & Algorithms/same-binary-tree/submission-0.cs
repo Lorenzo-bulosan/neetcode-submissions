bf10c5103e1b496b535fddbdcf5eb00591459100
Lorenzo Bulosan Bulosan <36127677+Lorenzo-bulosan@users.noreply.github.com>
2026-09-14 22:23:30 +0100

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+
+    
+
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        
+        // base case
+        if(p == null && q == null) return true;
+
+        if(p != null && q == null) return false;
+        if(p == null && q != null) return false;
+
+        // compare the head of each
+        var headIsSame = p.val == q.val; // if you return true here then we never get to check children
+
+        // traverse same children
+        return headIsSame && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+}
